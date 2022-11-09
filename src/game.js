25cbae99f00d9971c0e7164f51540bcbc7066881
Karrar Qasim <Karawr1996@gmail.com>
2022-11-09 10:42:28 -0700

    Add win conditions and draw condition

--- src/game.js
+++ src/game.js
@@ -25,6 +25,7 @@ class Game {
 
     checkPlayerTurn() { // determine currentTurn
         this.turnCount += 1;
+        this.checkDrawCondition();
         this.changeTurn();
     }
 
@@ -47,17 +48,28 @@ class Game {
 
     checkWinCondition() {
         // check hor/vert/diag/draw methods
+        var winningToken = "";
+        this.checkHorizontalWin();
+        this.checkVerticalWin();
+        this.checkDiagonalWin();
+
+        for (var i = 0; i < this.players.length; i++) {
+            winningToken = this.players[i].token;
+            this.players[i].winsCount += 1;
+            currentGame.gameOver = true;
+        }
     }
 
-    checkDrawCondition() {
-        // check for Draw conditions
-    }
-
-    checkHorizontalWin() {
-        // horizontal win conditions include
-        // [[0], [1], [2]]
-        // [[3], [4], [5]]
-        // [[6], [7], [8]]
+    checkHorizontalWin() { 
+        if (this.board.a1 === this.board.a2 && this.board.a2 === this.board.a3) {
+            return this.board.a1;
+        } else if (this.board.b1 === this.board.b2 && this.board.b2 === this.board.b3) {
+            return this.board.b1;
+        } else if (this.board.c1 === this.board.c2 && this.board.c2 === this.board.c3) {
+            return this.board.c1;
+        } else {
+            return "";
+        }
     }
 
     checkVerticalWin() {
@@ -65,12 +77,34 @@ class Game {
         // [[0], [3], [6]]
         // [[1], [4], [7]]
         // [[2], [5], [8]]
+        if (this.board.a1 === this.board.b1 && this.board.b1 === this.board.c1) {
+            winningToken = this.board.a1;
+        } else if (this.board.a2 === this.board.b2 && this.board.b2 === this.board.c2) {
+            winningToken = this.board.a2;
+        } else if (this.board.a3 === this.board.b3 && this.board.b3 === this.board.c3) {
+            winningToken = this.board.a3;
+        } else {
+            return "";
+        }
     }
 
     checkDiagonalWin() {
         // diagonal win conditions include
         // [[0], [4], [8]]
         // [[2], [4], [6]]
+        if (this.board.a1 === this.board.b2 && this.board.b2 === this.board.c3) {
+            winningToken = this.board.a1;
+        } else if (this.board.a3 === this.board.b2 && this.board.b2 === this.board.c1) {
+            winningToken = this.board.a3;
+        } else {
+            return "";
+        }
+    }
+
+    checkDrawCondition() {
+        if (this.turnCount > 8) {
+            currentGame.gameOver = true;
+        }
     }
 
     resetGame() {
